368525f34efc42ece5ef070e89f9243a57ddbc5c
marten <77919093+skriptum@users.noreply.github.com>
2022-02-08 16:52:09 +0100

    outlined the skeleton of the web app

--- dash/layout.py
+++ dash/layout.py
@@ -4,27 +4,37 @@ from dash import html
 def make_layout(draw):
 
     app_layout = html.Div(children=[
-        html.Div(
-            dcc.Graph(
-                figure=draw.choropleth(),
-                config={"displayModeBar":False}
-            )
-        ),
-        html.Div(
-            dcc.Graph(
-                figure=draw.bar_charts(),
-                config = {"responsive": False, "displayModeBar": False},
-            )
-        ),
-        html.Div(
-            dcc.Graph(
-                figure=draw.treemap(),
-                config = {"responsive": False, "displayModeBar": False},
-            )
-        ),
 
-        html.Div(
-            draw.user_profile("johannesvogel")
-        )
+        ## left side
+        html.Div(className = "four columns", children = [
+
+            html.Div(children = [
+                draw.user_profile("johannesvogel")
+            ])
+        ]),
+
+        ## right side
+        html.Div(className = "eight columns", children= [
+            html.Div(children = [
+
+                dcc.Graph(
+                    figure=draw.choropleth(),
+                    config={"displayModeBar":False}
+                )
+            ]),
+            html.Div(children = [
+                dcc.Graph(
+                    figure=draw.bar_charts(),
+                    config = {"responsive": False, "displayModeBar": False},
+                )
+            ]),
+            html.Div(
+                dcc.Graph(
+                    figure=draw.treemap(),
+                    config = {"responsive": False, "displayModeBar": False},
+                )
+            ),
+        ]),
+        
     ])
     return app_layout
